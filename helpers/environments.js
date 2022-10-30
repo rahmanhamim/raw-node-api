6aee4c46bbe3928e33c5d9b87b169578541633b9
rahmanhamim <rahmanhamiminfo@gmail.com>
2022-10-30 22:25:59 +0600

    env updated

---
+++ helpers/environments.js
@@ -0,0 +1,34 @@
+/** 
+@title :Environment
+@Description : Handle al environment
+@Author : HAMIM
+@Date : 30 Oct 2022
+*/
+
+// dependencies
+
+// module scaffolding
+const environments = {};
+
+environments.staging = {
+    port: 5000,
+    envName: "staging",
+};
+
+environments.production = {
+    port: 9000,
+    envName: "production",
+};
+
+// determine which environment was passed
+const currentEnvironment =
+    typeof process.env.NODE_ENV === "string" ? process.env.NODE_ENV : "staging";
+
+// export corresponding environment object
+const environmentToExport =
+    typeof environments[currentEnvironment] === "object"
+        ? environments[currentEnvironment]
+        : environments.staging;
+
+// export module
+module.exports = environmentToExport;
